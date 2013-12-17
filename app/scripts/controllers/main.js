c3fbd11a14ad38f76c2b203427561eb290b2c6f0
acramatte <acramatte@comerge.net>
2013-12-17 20:25:38 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('IntroductionAngularJSApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
